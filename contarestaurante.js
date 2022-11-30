cefd3c30304b97da001b85b14ab6b0df350503e4
jebenevenuto <jebenevenuto@gmail.com>
2022-11-30 18:38:00 -0400

    cria contarestaurante.js

---
+++ contarestaurante.js
@@ -0,0 +1,14 @@
+// let conta = 80;
+// let taxa = 80 * 0.1;
+// let total = conta + taxa;
+
+// console.log(total);
+
+let conta = 80;
+let taxaGarcom = 10;
+let total;
+
+total = 80 + ((taxaGarcom / 100) * 80);
+
+// console.log((taxaGarcom / 100) * 80);
+console.log(total);
