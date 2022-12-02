64298e8d23ec1f4bd45ae8a73abb55c2e3a3f59e
jebenevenuto <jebenevenuto@gmail.com>
2022-12-02 19:11:44 -0400

    Solve Me First HH

---
+++ solvemefirst-HH.js
@@ -0,0 +1,16 @@
+function solveMeFirst(a, b) {
+    // Hint: Type return a+b below   
+return a+b;
+
+  }
+  
+console.log();
+  
+function soma(a,b) {
+    return a+b;
+}
+
+soma(10,7);
+
+console.log(soma(10,7));
+console.log(soma(20,7));
