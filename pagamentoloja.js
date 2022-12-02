7de41ad332b0b9c28237cc9b9591d6f3c5cf962f
jebenevenuto <jebenevenuto@gmail.com>
2022-12-02 18:52:21 -0400

    pagamento loja

---
+++ pagamentoloja.js
@@ -0,0 +1,10 @@
+let preco = 60;
+let aVista = 10;
+let totalAvista = 0;
+
+totalAvista = (preco - (aVista / 100) * preco);
+let aPrazo = 
+
+console.log(`Preco R$ ${preco},00`);
+console.log(`Preço à vista R$ ${totalAvista},00`);
+console.log(`Preço parcelado R$ ${preco / 3},00`);
